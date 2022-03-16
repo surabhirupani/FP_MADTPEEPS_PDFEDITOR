f71cca35d9b85f45f93a927d99cad5a53cb41d26
SAI KIRAN REDDY POCHAMPALLY <c0836167@mylambton.ca>
2022-03-16 15:37:35 -0400

    add file for constants

---
+++ app/src/main/java/com/example/pdfeditormadtpeeps/Utility/Utils.java
@@ -0,0 +1,11 @@
+package com.example.pdfeditormadtpeeps.Utility;
+
+public class Utils {
+
+    public static final String APP_NAME = "PDFEDITOR";
+
+    //Login Utils
+    public static final String loginControlKey = "isLogin";
+    public static final String loginUserNameKey = "userName";
+    public static final String loginUserPassword = "userPassword";
+}
