d2389433e28aabea6bae6fcd062c1c148334a746
SAI KIRAN REDDY POCHAMPALLY <c0836167@mylambton.ca>
2022-03-16 17:50:43 -0400

    add database for files management

---
+++ app/src/main/java/com/example/pdfeditormadtpeeps/database/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.example.pdfeditormadtpeeps.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import com.example.pdfeditormadtpeeps.Utility.Utils;
+
+
+@Database(entities = {History.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, Utils.DATABASE_NAME)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract HistoryDao historyDao();
+}
