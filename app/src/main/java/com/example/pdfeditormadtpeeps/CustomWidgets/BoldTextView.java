5818a5c4b2417288f5209d902a1acd313aef9df1
SAI KIRAN REDDY POCHAMPALLY <c0836167@mylambton.ca>
2022-03-16 15:35:37 -0400

    Add files via upload
    add custom class for bold and thin textview

---
+++ app/src/main/java/com/example/pdfeditormadtpeeps/CustomWidgets/BoldTextView.java
@@ -0,0 +1,30 @@
+package com.example.pdfeditormadtpeeps.CustomWidgets;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+public class BoldTextView extends androidx.appcompat.widget.AppCompatTextView {
+
+    /*
+     * Caches typefaces based on their file path and name, so that they don't have to be created every time when they are referenced.
+     */
+    private static Typeface mTypeface;
+
+    public BoldTextView(final Context context) {
+        this(context, null);
+    }
+
+    public BoldTextView(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BoldTextView(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+
+        if (mTypeface == null) {
+            mTypeface = Typeface.createFromAsset(context.getAssets(), "fonts/PoppinsBold.ttf");
+        }
+        setTypeface(mTypeface);
+    }
+}
